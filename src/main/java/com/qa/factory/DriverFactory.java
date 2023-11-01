ed88f3662eb1b75fee2aeb81ae5127268abff07b
Sahil Kumar <sahil.kumar9274@gmail.com>
2023-11-01 09:06:08 +0530

    Added multi stages logic

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,9 +17,12 @@ public class DriverFactory {
 
     Properties properties;
 
+    FileInputStream fp = null;
+
     public WebDriver launchBrowser(Properties properties){
 
         String browserName = properties.getProperty("browser");
+        //String browserName = System.getProperty("browser");
         System.out.println("browser name is:: "+browserName);
 
         OptionsManager  optionsManager = new OptionsManager(properties);
@@ -46,22 +50,49 @@ public class DriverFactory {
     }
 
     public Properties initProp()  {
+
         //1. Tell where are config.properties file is located
         //2. To load the config.properties file
+        //3. Return properties instance
+
+
+        //mvn clean -Denv = ""
         properties = new Properties();
 
-        FileInputStream fp = null;
+       String envName =  System.getProperty("env");
+
+
+       try {
+           if (envName == null) {
+               System.out.println("env Name received as null, so I am running it on QA env");
+               fp = new FileInputStream("./src/test/resources/properties/qa.config.properties");
+           } else {
+               switch (envName.toLowerCase().trim()) {
+                   case "qa":
+                       fp = new FileInputStream("./src/test/resources/properties/qa.config.properties");
+                       break;
+                   case "stage":
+                       fp = new FileInputStream("./src/test/resources/properties/stage.config.properties");
+                       break;
+                   case "dev":
+                       fp = new FileInputStream("./src/test/resources/properties/dev.config.properties");
+                       break;
+                   case "prod":
+                       fp = new FileInputStream("./src/test/resources/properties/config.properties");
+                       break;
+                   default:
+                       System.out.println("please pass the correct value(envName)");
+               }
+           }
+       }
+       catch (FileNotFoundException e){
+           e.printStackTrace();
+       }
+
         try {
-            fp = new FileInputStream("./src/test/resources/properties/config.properties");
             properties.load(fp);
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        catch (Exception e){
-            e.printStackTrace();
         }
         return properties;
     }
